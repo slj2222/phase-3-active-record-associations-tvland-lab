78836d264970f4c1ceb4aad751595e3d08f132b0
Spencer Johnson <sljohnson2222@gmail.com>
2022-03-22 10:42:51 -0500

    commit tree 1cb9ae195464e31f72cbc5cce04c79efad62a4ce

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    # returns a list of roles for the actor using the character name and show name
+    
+    self.characters.map do |character|
+        "#{characters.name} - #{characters.show.name}"
+    end
+  end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Show.create(name: "GOT", day: "Sunday", season: "one", genre: "fantasy")
+
+Actor.create(first_name: "Emilia", last_name: "Clarke")
+Actor.create(first_name: "Sophie", last_name: "Turner")
+
+Character.create(name: "Daenerys Targaryen", actor_id: 1, show_id: 1, catchphrase: "firee")
+Character.create(name: "Sansa", actor_id: 2, show_id: 1, catchphrase: "okay")
